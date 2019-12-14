198d6ddb4a5f3cd6d8850fd53c34ff6b5ca0aa44
unknown <alberto.bosch.vigo@gmail.com>
2019-12-14 23:53:32 +0100

    fixed connection error

--- conexionDB.py
+++ conexionDB.py
@@ -2,19 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 def main():
-    datos_conexion = r'postgresql://postgres:mcspI%1705@127.0.0.1/cursoWebCS50'
-    print(datos_conexion)
+    datos_conexion = 'postgresql://postgres:12345678@localhost/cursoWebCS50'
+    #print(datos_conexion)
     engine = create_engine(datos_conexion)
-    print(engine)
-    #sesion = scoped_session(sessionmaker(bind=engine))
-    #resultado = sesion.execute("SELECT * FROM usuarios")
+    #print(engine)
+    sesion = scoped_session(sessionmaker(bind=engine))
+    resultado = sesion.execute("SELECT * FROM usuarios")
+    """
     try:
         conexion = engine.connect()
     except Exception as e:
         print(e)
         return False
+    
     resultado = conexion.execute("select * from usuarios")
-    print(resultado)
+    """
+    #print(resultado)
     for linea in resultado:
         print(f'{ linea.id }, {linea.usuario }, { linea.password }')
 
