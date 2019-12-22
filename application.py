52466ab6f502c44065d1bdeb5c5e3c915e088879
unknown <alberto.bosch.vigo@gmail.com>
2019-12-22 22:49:50 +0100

    proyecto funcional

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ from conexionDB import conectorDB
     {% block nombreDelBloque %}
     {% endblock nombreDelBloque %}
 """
-listaPaginas = ['tradicionales', 'exoticas', 'pan']
+listaPaginas = ['tradicionales', 'exoticas', 'pan', 'notas']
 listaNotas = []
 app = Flask(__name__)
 app.config['SESSION_PERMANENT'] = False
@@ -35,15 +35,23 @@ def index():
     return render_template('index.html', headline = headline, listaPaginas = listaPaginas)
 
 @app.route('/<string:name>')
-def paginaDefault(name = 'defecto'):
-    print(name)
+def contenido(name = 'defecto'):
     if name in listaPaginas:
-        return render_template('contenido.html', headline = name)
+        listaAux = listaPaginas.copy()
+        listaAux.remove(name)
+        """
+        for i in listaPaginas:
+            if i != name:
+                listaAux.append(i)
+        """        
+        return render_template('contenido.html', headline = name, listaPaginas = listaAux)
     else:
         return render_template('default_error.html', headline = 'Pagina %s desconocida' % name)
 
 @app.route('/notas', methods = ['GET', 'POST'])
 def notas():
+    listaAux = listaPaginas.copy()
+    listaAux.remove('notas')
     if session.get('notas') is None:
         session['notas'] = []
     if request.method == 'POST':
@@ -55,7 +63,7 @@ def notas():
             else:
                 session['notas'].append(nota)
     
-    return render_template('notas.html', notas = session['notas'], headline = 'Notas')
+    return render_template('notas.html', notas = session['notas'], headline = 'Notas', listaPaginas = listaAux)
 
 
 if __name__ == '__main__':
